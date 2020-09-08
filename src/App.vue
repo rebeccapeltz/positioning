e6efa950f4153b5631a33f7a2cefcebc687db71c
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-09-07 18:54:02 -0700

    fix mobile form

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,13 @@
       <img :src="url" alt="positioning example" />
     </div>
 
-    <b-form @submit="updateImage" inline>
+    <b-form @submit="updateImage" class="form-inline">
       <label class="sr-only" for="gravity">Gravity:</label>
 
       <b-form-select
         id="gravity"
         v-model="gravity"
         :options="compass"
-        class="mb-2 mr-sm-2 mb-sm-0"
         required
       ></b-form-select>
       <label for="xpos">x:</label>
@@ -23,7 +22,6 @@
         step="10"
         id="xpos"
         v-model="xpos"
-        class="mb-2 mr-sm-2 mb-sm-0"
         placeholder="X position"
       ></b-form-input>
 
@@ -35,14 +33,13 @@
         step="10"
         id="ypos"
         v-model="ypos"
-        class="mb-2 mr-sm-2 mb-sm-0"
         placeholder="Y position"
       ></b-form-input>
       <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
     <div class="parent">
-      <div v-show="url" class="url-display" >
-          <a :href="url" target="_blank">{{ url }}</a>
+      <div v-show="url" class="url-display">
+        <a :href="url" target="_blank">{{ url }}</a>
       </div>
     </div>
   </div>
@@ -124,19 +121,19 @@ export default {
   margin-top: 4rem;
 }
 
-label,
+/* label,
 button {
   margin: 0 0.5rem;
-}
+} */
 #tranform {
   width: 300;
 }
 
 .parent {
   display: flex;
-  justify-content: center; 
-   align-items: center;
-   padding: 5px;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
 }
 .url-display {
   padding: 1rem;
@@ -151,5 +148,72 @@ button {
 }
 .url-display > div {
   min-width: 0;
+}
+.form-inline {
+  display: flex;
+  justify-content: center;
+  flex-flow: row wrap;
+  align-items: center;
+}
+
+.form-inline label {
+  margin: 0;
+  font-size: 1rem;
+  color: #0e2f5a;
+}
+
+.form-inline input {
+  vertical-align: middle;
+  margin: 0;
+  /* padding: 0.6rem; */
+  background-color: #fff;
+  border: 0.3rem solid #ddd;
+}
+
+.form-inline button {
+  padding: 0.3rem 1.2rem;
+  background-color: #0e2f5a;
+  border: 1px solid #ddd;
+  color: white;
+  cursor: pointer;
+}
+.form-inline button:disabled {
+  background-color: gray;
+  cursor: none;
+}
+.form-inline button:hover {
+  background-color: royalblue;
+}
+
+@media only screen and (min-width: 800px) {
+  .form-inline label {
+    margin: 0.3rem;
+  }
+  .form-inline button {
+    margin: 0 0 0 0.3rem;
+  }
+  .form-inline button:disabled {
+    background-color: gray;
+    cursor: none;
+  }
+
+  .form-inline input {
+    margin: 0.3rem 0;
+  }
+}
+@media (max-width: 800px) {
+  .form-inline {
+    flex-direction: column;
+    align-items: stretch;
+  }
+  .form-inline button {
+    margin-top: 0.3rem;
+  }
+  h1 {
+    font-size: 1.3rem;
+  }
+  h2 {
+    font-size: 1rem;
+  }
 }
 </style>
